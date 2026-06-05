75612a70cdc29ccc8262154b075883c01dd346c6
Arturo <29128253+aig77@users.noreply.github.com>
2026-06-05 16:45:00 -0400

    chore: update config for database url

--- src/config.rs
+++ src/config.rs
@@ -1,7 +1,6 @@
 pub struct Config {
     pub addr: String,
-    pub db_path: String,
-    pub db_url: String,
+    pub database_url: String,
 }
 
 impl Config {
@@ -9,9 +8,8 @@ impl Config {
         dotenvy::dotenv().ok();
         Config {
             addr: std::env::var("ADDRESS").expect("Missing required env var ADDRESS"),
-            db_path: std::env::var("DATABASE_PATH")
-                .expect("Missing required env var DATABASE_PATH"),
-            db_url: std::env::var("DATABASE_URL").expect("Missing required env var DATABASE_URL"),
+            database_url: std::env::var("DATABASE_URL")
+                .expect("Missing required env var DATABASE_URL"),
         }
     }
 }
