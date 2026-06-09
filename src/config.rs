d2e3dea1a48d112f4e77e6e0340657ba6bb77357
Arturo <29128253+aig77@users.noreply.github.com>
2026-06-09 18:34:43 -0400

    feat: create resend account and add support for api key

--- src/config.rs
+++ src/config.rs
@@ -3,17 +3,20 @@ pub struct Config {
     pub addr: String,
     pub base_url: String,
     pub database_url: String,
+    pub resend_api_key: String,
 }
 
 impl Config {
     pub fn from_env() -> Self {
         dotenvy::dotenv().ok();
         Config {
-            addr: std::env::var("ADDRESS").expect("Missing required env var ADDRESS"),
+            addr: std::env::var("ADDRESS").unwrap_or_else(|_| "127.0.0.1:3000".to_string()),
             base_url: std::env::var("BASE_URL")
                 .unwrap_or_else(|_| "http://127.0.0.1:3000".to_string()),
             database_url: std::env::var("DATABASE_URL")
                 .expect("Missing required env var DATABASE_URL"),
+            resend_api_key: std::env::var("RESEND_API_KEY")
+                .expect("Missing required env var RESEND_API_KEY"),
         }
     }
 }
